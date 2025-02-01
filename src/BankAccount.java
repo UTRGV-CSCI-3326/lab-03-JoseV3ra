6326935397bdaf2aaf1b720234f842fb71fe82c0
JoseV3ra <143547576+JoseV3ra@users.noreply.github.com>
2025-02-01 16:17:15 +0000

    First Commit

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,1 +1,22 @@
+public class BankAccount
+{
+
+    public static void main (String [] args)
+{
+    
+   double x = 2175.37;
+    x -= 302.50;
+    x += 50.03;
+    x -= x / 2; 
+    x += 20.00;
+    x -= 1.00; 
+    x += x;
+    x += 1.00;
+
+    x = Math.round(x * 100.0) / 100.0;
+
+System.out.print("Bob's final balance: " + x);
+    }
+}    
+
 
